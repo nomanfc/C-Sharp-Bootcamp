01a449eb9e545e6bd10ab3d46e86a2a38da25a1c
nomanfc <nomanfcb@gmail.com>
2023-10-09 12:49:14 +0600

    some project name has been renamed

---
+++ Learning_dsa_in_csharp/Learning_dsa_in_csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LearningC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           MyCustomStack.MyStack();
+           Console.ReadKey();
+        }
+    }
+}
