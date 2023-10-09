08543c87c5be019b030159b8e56dc6f2b81c5229
nomanfc <nomanfcb@gmail.com>
2023-10-09 20:26:08 +0600

    Voting algo is updated

--- Learning_dsa_in_csharp/Algorithm_in_csharp/Algorithm_in_csharp/Program.cs
+++ Learning_dsa_in_csharp/Algorithm_in_csharp/Algorithm_in_csharp/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Boyer_Moore_Voting_algo;
+
+Console.WriteLine("::::::::::::::::::::::::!! Algorithms !!::::::::::::::::::::::");
+
+Console.WriteLine("\n\n1. Boyer Moore Voting Algorithm\n");
+int[] number = { 0, 0, 0, 0, 0, 2, 2, 0, 1, 2, 1, 0 };
+Console.WriteLine("Array : [0, 0, 0, 0, 0, 2, 2, 0, 1, 2, 1, 0]");
+int winner = BoyerMoreVotingAlgo.VotingAlgo(number);
+if (winner > -1)
+{
+    Console.WriteLine($"Winner is : {winner}");
+}
+else
+{
+    Console.WriteLine("No Winner Found!!");
+}
+
+
+Console.ReadLine();
--- Learning_dsa_in_csharp/Boyer_Moore_Voting_algo/Boyer_Moore_Voting_algo/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Boyer_Moore_Voting_algo;
-
-Console.WriteLine("Boyer Moore Voting Algo");
-int[] numbers = { 11,22,11,22,11,11,11,22 };
-Console.WriteLine($"Array is :  [11,22,11,22,11,11,11,22]  \n{BoyerMoreVotingAlgo.VotingAlgo(numbers)} is the WINNER!!\n\n");
