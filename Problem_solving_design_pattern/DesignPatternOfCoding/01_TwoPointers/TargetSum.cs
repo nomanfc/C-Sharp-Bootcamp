09926d0591145d279430ee62a497f1cde7d3622b
nomanfc <nomanfcb@gmail.com>
2023-10-12 20:34:12 +0600

    TwoSum Pointers Pattern added

---
+++ Problem_solving_design_pattern/DesignPatternOfCoding/01_TwoPointers/TargetSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternOfCoding
+{
+    internal class TwoPointers
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+
+            while (left < right)
+            {
+                if (numbers[left] + numbers[right] < target) left++;
+                else if (numbers[left] + numbers[right] > target)    right--;
+                else return new int[] { left+1, right+1};
+            }
+
+            return new int[2];
+        }
+    }
+}
---
+++ Problem_solving_design_pattern/DesignPatternOfCoding/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesignPatternOfCoding;
+
+//TwoPointers Sum Problem
+Console.WriteLine("01. Two Sum Problem");
+int[] arr = { 1, 3, 4, 4, 5, 6, 7, 8, 9, 10, 10 };
+int target = 19;
+int[] result = TwoPointers.TwoSum(arr, target);
+Console.WriteLine($"Geiven Array: [1, 3, 4, 4, 5, 6, 7, 8, 9, 10, 10] and Target is: {target}" );
+Console.Write("Output: [");
+foreach (int i in result)
+{
+    Console.Write(" " + i + " ");
+}
+Console.Write("]\n\n");
