7a17f436c317366fa6712e5da379137e7cfaf16c
nomanfc <nomanfcb@gmail.com>
2023-10-13 18:38:43 +0600

    Assignment 1: Calculator submitted

--- Learning_csharp_lang/FundamentalCS/src/Fundamental_Assignment_Calculator/Program.cs
+++ Learning_csharp_lang/FundamentalCS/src/Fundamental_Assignment_Calculator/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine(":::::::::::::::::::::- CALCULATOR -:::::::::::::::::");
+
+Console.Write("Enter First Number: ");
+string inputOne = Console.ReadLine();
+Console.Write("Enter Second Number: ");
+string inputTwo = Console.ReadLine();
+
+float numberOne = float.Parse(inputOne);
+float numberTwo = float.Parse(inputTwo);
+
+Console.WriteLine("\nSelect an Operation: ");
+Console.WriteLine("[A]-ADDITION");
+Console.WriteLine("[S]-SUBTRACTION");
+Console.WriteLine("[M]-MULTIPLICATION");
+Console.WriteLine("[D]-DIVISION");
+Console.WriteLine("[R]-REMINDER");
+
+
+Console.Write("\nEnter Options: ");
+string operation = Console.ReadLine();
+
+if (operation == "A" || operation == "a") Console.WriteLine("ANS: " + Addition(numberOne, numberTwo));
+else if (operation == "S" || operation == "s") Console.WriteLine("ANS: " + Subtraction(numberOne, numberTwo));
+else if (operation == "M" || operation == "m") Console.WriteLine("ANS: " + Multiplication(numberOne, numberTwo));
+else if (operation == "D" || operation == "d") Console.WriteLine("ANS: " + Division(numberOne, numberTwo));
+else if (operation == "R" || operation == "r") Console.WriteLine( "ANS: " + Modulo(numberOne, numberTwo));
+else InvalidOpearation();
+
+
+float Addition(float a, float b)
+{
+    return a + b;
+}
+
+float Subtraction(float a, float b)
+{
+    return a - b;
+}
+
+float Multiplication(float a, float b)
+{
+    return a * b;
+}
+
+float Division(float a, float b)
+{
+    return a / b;
+}
+
+float Modulo(float a, float b)
+{
+    return a % b;
+}
+
+void InvalidOpearation()
+{
+    Console.WriteLine("Invalid Operation");
+}
+
+
+
