5928ddc1704170588e3c8077ffad698ca040bdcb
nomanfc <nomanfcb@gmail.com>
2023-10-15 12:30:07 +0600

    Project ToDo: Validation added in option choice

--- Learning_csharp_lang/FundamentalCS/src/Fundamental_Final_Poject_ToDoList/Program.cs
+++ Learning_csharp_lang/FundamentalCS/src/Fundamental_Final_Poject_ToDoList/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine(" ::::::::::::::::::::-ToDo List Application-::::::::::::::::::::::");
+﻿Console.WriteLine(" ::::::::::::::::::::- ToDo List Application -::::::::::::::::::::::");
 
 Console.WriteLine("\n Hello, welcome to Todo Application..\n");
 Console.WriteLine(" What do you want to do?\n");
@@ -8,7 +8,45 @@ Console.WriteLine(" [R]emove a TODO");
 Console.WriteLine(" [E]xit");
 
 Console.Write("\n Choose an option: ");
-var chooseOption = Console.Read(); 
+
+string chooseOption;
+bool varification;
+
+chooseOption = Console.ReadLine();
+varification = isInsertedOptionValid(chooseOption);
+
+while (!varification)
+{
+    Console.Write("\n Invalid Option. Please Enter valid Option: ");
+    chooseOption = Console.ReadLine();
+    varification = isInsertedOptionValid(chooseOption);
+}
+
+
+
+
+
+//Methods here
+
+//checking choosed option is valid or not
+static bool isInsertedOptionValid(string option)
+{
+    string op = option.ToUpper();
+    switch (op)
+    {
+        case "S":
+            return true;
+        case "A":
+            return true;
+        case "R":
+            return true;
+        case "E":
+            return true;
+        default:
+            return false;
+    }
+}
+
 
 
 
