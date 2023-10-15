086bbe04113f896b4a6c8c3765dd791a0f894780
nomanfc <nomanfcb@gmail.com>
2023-10-15 11:57:42 +0600

    Project ToDo: Option choice has been added

--- Learning_csharp_lang/FundamentalCS/src/Fundamental_Final_Poject_ToDoList/Program.cs
+++ Learning_csharp_lang/FundamentalCS/src/Fundamental_Final_Poject_ToDoList/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.WriteLine("::::::::::::::::::::-ToDo List Application-::::::::::::::::::::::");
+﻿Console.WriteLine(" ::::::::::::::::::::-ToDo List Application-::::::::::::::::::::::");
+
+Console.WriteLine("\n Hello, welcome to Todo Application..\n");
+Console.WriteLine(" What do you want to do?\n");
+Console.WriteLine(" [S]ee all TODOs");
+Console.WriteLine(" [A]dd new TODO");
+Console.WriteLine(" [R]emove a TODO");
+Console.WriteLine(" [E]xit");
+
+Console.Write("\n Choose an option: ");
+var chooseOption = Console.Read(); 
 
 
 
