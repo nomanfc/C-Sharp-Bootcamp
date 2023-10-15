eef9d9f878a600d4457880e66a1b2dc3868f7c46
nomanfc <nomanfcb@gmail.com>
2023-10-15 13:32:05 +0600

    Project ToDo: MainApp Method added

--- Learning_csharp_lang/FundamentalCS/src/Fundamental_Final_Poject_ToDoList/Program.cs
+++ Learning_csharp_lang/FundamentalCS/src/Fundamental_Final_Poject_ToDoList/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine(" ::::::::::::::::::::- ToDo List Application -::::::::::::::::::::::");
+﻿using System.Security.Cryptography;
+
+Console.WriteLine(" ::::::::::::::::::::- ToDo List Application -::::::::::::::::::::::");
 
 Console.WriteLine("\n Hello, welcome to Todo Application..\n");
 Console.WriteLine(" What do you want to do?\n");
@@ -7,29 +9,40 @@ Console.WriteLine(" [A]dd new TODO");
 Console.WriteLine(" [R]emove a TODO");
 Console.WriteLine(" [E]xit");
 
-Console.Write("\n Choose an option: ");
-
 string chooseOption;
 bool varification;
+List<string> todoList = new();
 
-chooseOption = Console.ReadLine();
-varification = isInsertedOptionValid(chooseOption);
-
-while (!varification)
-{
-    Console.Write("\n Invalid Option. Please Enter valid Option: ");
-    chooseOption = Console.ReadLine();
-    varification = isInsertedOptionValid(chooseOption);
-}
-
-
-
-
+MainApp();
 
 //Methods here
 
+void MainApp()
+{
+    Console.Write("\n Choose an option: ");
+    chooseOption = Console.ReadLine();
+    varification = isInsertedOptionValid(chooseOption.ToUpper());
+
+    CheckVerification();
+    Console.WriteLine("Main");
+    switch (chooseOption.ToUpper())
+    {
+        case "S":
+            DisplayTodo();
+            break;
+
+        case "A":
+            AddItem();
+            break;
+
+        default:
+            Console.WriteLine("Hi  ads");
+            break;
+    }
+}
+
 //checking choosed option is valid or not
-static bool isInsertedOptionValid(string option)
+bool isInsertedOptionValid(string option)
 {
     string op = option.ToUpper();
     switch (op)
@@ -46,6 +59,57 @@ static bool isInsertedOptionValid(string option)
             return false;
     }
 }
+
+//display todo
+void DisplayTodo()
+{
+    if (todoList.Count == 0)
+    {
+        Console.WriteLine("Todo List is empty");
+    }
+    else
+    {
+        foreach(string todo in todoList)
+        {
+            Console.WriteLine($"{todoList.IndexOf(todo)+1}.{todo}");
+        }
+    }
+}
+
+//add item
+void AddItem()
+{
+    Console.WriteLine("Enter Description: ");
+    string item = Console.ReadLine();
+
+    if (item.Length <= 0)
+    {
+        Console.WriteLine("Item must not be null");
+    }
+    else if(todoList.Contains(item))
+    {
+        Console.WriteLine("Item already exist in the list.");
+    }
+    else
+    {
+        todoList.Add(item);
+    }
+
+    MainApp();
+}
+
+//check verification
+void CheckVerification()
+{
+    while (!varification)
+    {
+        Console.Write("\n Invalid Option. Please Enter valid Option: ");
+        chooseOption = Console.ReadLine();
+        varification = isInsertedOptionValid(chooseOption);
+    }
+
+}
+
 
 
 
