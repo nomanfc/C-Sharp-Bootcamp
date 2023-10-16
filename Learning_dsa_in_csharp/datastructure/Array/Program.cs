eec21a64435e36e149752bf55dd6f178cbb6ea6d
nomanfc <nomanfcb@gmail.com>
2023-10-16 21:28:59 +0600

    Array: program file has been updated

--- Learning_dsa_in_csharp/datastructure/Array/Program.cs
+++ Learning_dsa_in_csharp/datastructure/Array/Program.cs
@@ -2,7 +2,11 @@
 
 Console.WriteLine("::::::::::::::::::::::- ARRAY -:::::::::::::::::::::::");
 
+int[] array = new int[6];   
+
 Console.WriteLine("\n01.Insert At End Of an Array: ");
-InsertAtArrayEnd.Main();
+InsertAtArrayEnd.Main(array);
+
+
 
 Console.ReadKey();
