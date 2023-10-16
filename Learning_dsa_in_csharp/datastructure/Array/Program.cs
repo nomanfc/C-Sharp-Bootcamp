5327bf0c08253685916d7099a67121d7aaca79d8
nomanfc <nomanfcb@gmail.com>
2023-10-16 22:21:19 +0600

    Array: program file updated

--- Learning_dsa_in_csharp/datastructure/Array/Program.cs
+++ Learning_dsa_in_csharp/datastructure/Array/Program.cs
@@ -13,8 +13,9 @@ length = InsertAtArrayEnd.Main(array, length, 100);
 Console.WriteLine("\n02.Insert At Start Of an Array: ");
 length = InsertAtArrayStart.Main(array, length, 120);
 
-
-
+Console.WriteLine("\n03.Insert At Any Position Of an Array: ");
+length = InsertAtGivenArrayPosition.Main(array, length, 10, 120);
+if(length == 404) { Console.WriteLine("Length out of bound"); }
 
 Console.WriteLine("Length: " + length);
 Console.ReadKey();
