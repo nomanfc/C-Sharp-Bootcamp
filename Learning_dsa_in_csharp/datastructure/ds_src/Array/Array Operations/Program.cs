06ed9af8b19a567f68f40fff089e7de2f895ad22
nomanfc <nomanfcb@gmail.com>
2023-10-17 20:13:16 +0600

    Array: program cs file has been updated

--- Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Operations/Program.cs
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Operations/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Array;
+using Array_Operations;
 
 Console.WriteLine("::::::::::::::::::::::- ARRAY -:::::::::::::::::::::::");
 
@@ -7,13 +8,26 @@ int[] array = new int[10];
 
 Console.WriteLine("\n01.Insert At End Of an Array: ");
 length = InsertAtArrayEnd.Main(array, length, 100);
+Console.WriteLine("\n\nLength Now: " + length + "\n");
 
 Console.WriteLine("\n02.Insert At Start Of an Array: ");
 length = InsertAtArrayStart.Main(array, length, 120);
-
-Console.WriteLine("\n03.Insert At Any Position Of an Array: ");
-length = InsertAtGivenArrayPosition.Main(array, length, 0, 120);
-if(length == 404) { Console.WriteLine("Length out of bound"); }
-
 Console.WriteLine("\n\nLength Now: " + length + "\n");
+
+Console.WriteLine("\n03.Insert At Any Position Of an Array (3): ");
+length = InsertAtGivenArrayPosition.Main(array, length, 3, 1200);
+Console.WriteLine("\n\nLength Now: " + length + "\n");
+
+Console.WriteLine("\n04.Remove from End Of an Array: ");
+length = RemoveLastArrayElement.Main(array, length);
+Console.WriteLine("\n\nLength Now: " + length + "\n");
+
+Console.WriteLine("\n05.Remove from Start Of an Array: ");
+length = RemoveFirstArrayElement.Main(array, length);
+Console.WriteLine("\n\nLength Now: " + length + "\n");
+
+Console.WriteLine("\n06.Remove From Any Position Of an Array (3): ");
+length = RemoveFromGivenArrayPosition.Main(array, length, 3);
+Console.WriteLine("\n\nLength Now: " + length + "\n");
+
 Console.ReadKey();
