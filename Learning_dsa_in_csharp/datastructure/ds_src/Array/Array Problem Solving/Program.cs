4bee35dc11102c16aac2e6fe73402961b4a492da
nomanfc <nomanfcb@gmail.com>
2023-10-17 14:49:11 +0600

    Array project structure updated

--- Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/Program.cs
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/Program.cs
@@ -8,4 +8,11 @@ Console.WriteLine($"Given Array: [0,1,5,3,0,1,4,0].\nExpected Output: [0,0,1,5,3
 Console.Write("Output: ");
 DuplicateZeros.DuplicateZero(array);
 
+Console.WriteLine("\n\n02. Merge Two Sorted Array");
+int[] array1 = new int[8] {1,2,3,4,5,0,0,0};
+int[] array2 = new int[3] {3,4,5};
+int m = 5, n = 3;
+Console.WriteLine($"Given Array1: [1,2,3,4,5,0,0,0].\nGiven Array2: [3,4,5].\nExpected Output: [1,2,3,3,4,4,5,5]");
+MergeTwoSortedArray.MergeSortedArray(array1, m, array2, n);
+
 Console.ReadKey();
