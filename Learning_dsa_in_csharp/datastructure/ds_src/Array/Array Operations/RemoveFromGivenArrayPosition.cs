f9b097e77f362314e39baaf59035fdf0884b670e
nomanfc <nomanfcb@gmail.com>
2023-10-17 20:12:40 +0600

    Array: Remove fom any given position code added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Operations/RemoveFromGivenArrayPosition.cs
@@ -0,0 +1,31 @@
+﻿using Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Operations
+{
+    internal class RemoveFromGivenArrayPosition
+    {
+        public static int Main(int[] array, int length, int position)
+        {
+            if (length > array.Length || position >= length) return length;
+
+
+            for(int i = position; i<length; i++)
+            {
+                array[i-1] = array[i];
+            }
+
+            length--;
+
+
+
+            Utility.DisplayArray(array, length);
+
+            return length;
+        }
+    }
+}
