371a845d0944935a266f0bf5dc02132c426f9174
nomanfc <nomanfcb@gmail.com>
2023-10-17 14:48:50 +0600

    ArrayCP: Merge Two sorted array added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/MergeTwoSortedArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class MergeTwoSortedArray
+    {
+        public static void MergeSortedArray(int[] nums1, int m, int[] nums2, int n)
+        {
+            int lastIndex = m + n - 1;
+            int i = m - 1;
+            int j = n - 1;
+
+            while (j >= 0)
+            {
+                if(i>=0 && nums1[i] > nums2[j])
+                {
+                    nums1[lastIndex] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[lastIndex] = nums2[j];
+                    j--;
+                }
+
+                lastIndex--;
+            }
+
+            Utility.Display(nums1);
+        }
+    }
+}
