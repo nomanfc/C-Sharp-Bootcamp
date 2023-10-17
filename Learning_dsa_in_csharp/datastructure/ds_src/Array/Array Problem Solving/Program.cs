f643a373c5201cc8717959780457cc508e9c92d7
nomanfc <nomanfcb@gmail.com>
2023-10-17 14:33:55 +0600

    Array Problem Solving project created

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/Program.cs
@@ -0,0 +1,11 @@
+﻿using Array_Problem_Solving;
+
+Console.WriteLine("::::::::::::::::::::- Problem Solving With Array -:::::::::::::::::::::");
+
+Console.WriteLine("\n01. Add Duplicate Zero");
+int[] array = new int[8] {0,1,5,3,0,1,4,0};
+Console.WriteLine($"Given Array: [0,1,5,3,0,1,4,0].\nExpected Output: [0,0,1,5,3,0,0,1]");
+Console.Write("Output: ");
+DuplicateZeros.DuplicateZero(array);
+
+Console.ReadKey();
