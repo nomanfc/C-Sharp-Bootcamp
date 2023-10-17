9d5e82ac40779ba996f8902acdbf486c9a8295ea
nomanfc <nomanfcb@gmail.com>
2023-10-17 22:22:21 +0600

    ArrayCP: Remove Given Element solution

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/RemovegivenElementFromArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class RemovegivenElementFromArray
+    {
+        public static void Main(int[] array, int value)
+        {
+            int count = 0;
+
+            for(int i = 0; i<array.Length; i++)
+            {
+                if (array[i] != value)
+                {
+                    array[count] = array[i];
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
