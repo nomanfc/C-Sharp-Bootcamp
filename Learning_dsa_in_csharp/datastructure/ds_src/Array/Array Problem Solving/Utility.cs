a590e83c8cfe8a611bff36de261a01c4ec751297
nomanfc <nomanfcb@gmail.com>
2023-10-17 14:48:27 +0600

    ArrayCP: Utility Class Added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    public class Utility
+    {
+        public static void Display(int[] array)
+        {
+            Console.Write("Output: ");
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
