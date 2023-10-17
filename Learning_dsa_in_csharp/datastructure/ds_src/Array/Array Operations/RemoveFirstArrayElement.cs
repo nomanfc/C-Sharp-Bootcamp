84559c52d13fd0a1d52a4ff992e07944ab0eaeb7
nomanfc <nomanfcb@gmail.com>
2023-10-17 19:49:15 +0600

    Array: Remove first element method name changed

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Operations/RemoveFirstArrayElement.cs
@@ -0,0 +1,26 @@
+﻿using Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Operations
+{
+    internal class RemoveFirstArrayElement
+    {
+        public static int Main(int[] array, int length)
+        {
+            for(int i = 1; i<length; i++)
+            {
+                array[i-1] = array[i];
+            }
+
+            length--;
+
+            Utility.DisplayArray(array, length);
+
+            return length;
+        }
+    }
+}
