85b1c0bd7017d0fba205988ae320f51fe281f6d4
nomanfc <nomanfcb@gmail.com>
2023-10-17 14:20:56 +0600

    Array solution folders has been rearranged

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using Array;
+
+Console.WriteLine("::::::::::::::::::::::- ARRAY -:::::::::::::::::::::::");
+
+int length = 0;
+int[] array = new int[10];
+
+
+
+Console.WriteLine("\n01.Insert At End Of an Array: ");
+length = InsertAtArrayEnd.Main(array, length, 100);
+
+Console.WriteLine("\n02.Insert At Start Of an Array: ");
+length = InsertAtArrayStart.Main(array, length, 120);
+
+Console.WriteLine("\n03.Insert At Any Position Of an Array: ");
+length = InsertAtGivenArrayPosition.Main(array, length, 0, 120);
+if(length == 404) { Console.WriteLine("Length out of bound"); }
+
+Console.WriteLine("Length: " + length);
+Console.ReadKey();
