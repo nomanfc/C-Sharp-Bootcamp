561a15572bcdc1aecb9a0f8944df98c1e704547f
nomanfc <nomanfcb@gmail.com>
2023-10-17 22:23:12 +0600

    ArratCP: Program cs file updated

--- Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/Program.cs
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/Program.cs
@@ -15,4 +15,17 @@ int m = 5, n = 3;
 Console.WriteLine($"Given Array1: [1,2,3,4,5,0,0,0].\nGiven Array2: [3,4,5].\nExpected Output: [1,2,3,3,4,4,5,5]");
 MergeTwoSortedArray.MergeSortedArray(array1, m, array2, n);
 
+Console.WriteLine("\n\n03. Remove Given Value");
+int[] arrayThree = new int[8] { 0, 1, 5, 3, 0, 1, 4, 0 };
+int value = 0;
+Console.WriteLine($"Given Array: [0,1,5,3,0,1,4,0].\nGiven Value: {value}.\nExpected Output: 5");
+Console.Write("Output: ");
+RemovegivenElementFromArray.Main(arrayThree, value);
+
+Console.WriteLine("\n\n04. Remove Duplicate Value");
+int[] arrayFour = new int[3] { 1,1,2 };
+Console.WriteLine($"Given Array: [1,1,2].\nExpected Output: 2");
+Console.Write("Output: ");
+RemoveDuplicateValue.Main(arrayFour);
+
 Console.ReadKey();
