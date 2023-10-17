2ebb66fbc7973ef647a6b43152a51890399250ea
nomanfc <nomanfcb@gmail.com>
2023-10-17 14:34:26 +0600

    ArrayCP: Duplicate Zero added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/DuplicateZeros.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class DuplicateZeros
+    {
+        public static void DuplicateZero(int[] nums)
+        {
+            List<int> newArray = new();
+
+            foreach (int i in nums)
+            {
+                if (i != 0)
+                {
+                    newArray.Add(i);
+                }
+                else
+                {
+                    newArray.Add(i);
+                    newArray.Add(i);
+                }
+            }
+
+            int[] resArray = newArray.ToArray();
+
+            Array.Copy(resArray, nums, nums.Length);
+
+            foreach (int i in nums)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
