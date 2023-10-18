9f48bdae1dd508894b753dcff611c5fd24e205fc
nomanfc <nomanfcb@gmail.com>
2023-10-18 21:28:58 +0600

    ArrayCP: Valid Mountain array code added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/ValidMountain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class ValidMountain
+    {
+        public static bool Main(int[] array)
+        {
+            if (array.Length < 3) return false;
+
+            int climb = 0;
+            
+            while(climb+1 < array.Length && array[climb] < array[climb+1]) 
+            {
+                climb++;
+            }
+
+            if(climb == 0 || climb == array.Length) return false;
+
+            while(climb+1 < array.Length && array[climb] > array[climb + 1])
+            {
+                climb++;
+            }
+
+            return climb == array.Length;
+        }
+    }
+}
