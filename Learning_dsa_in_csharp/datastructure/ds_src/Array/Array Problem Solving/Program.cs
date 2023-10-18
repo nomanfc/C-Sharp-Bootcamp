b0cd36319482be18241bc96fa9f06bd26fe4f28d
nomanfc <nomanfcb@gmail.com>
2023-10-18 21:29:21 +0600

    ArrayCP: program file has been updated

--- Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/Program.cs
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/Program.cs
@@ -27,4 +27,27 @@ Console.WriteLine($"Given Array: [1,1,2,2,3,5,7,7,7,8].");
 Console.Write("Output: ");
 RemoveDuplicateValue.Main(arrayFour);
 
+Console.WriteLine("\n\n05. Search if N and It's Double Exist");
+int[] arrayFive = new int[6] { 0,2,9,5,3,11 };
+Console.WriteLine($"Given Array: [0,2,9,5,3,11].");
+Console.Write("Output: ");
+Console.WriteLine(SearchingDoubleNumber.Main(arrayFive));
+Console.WriteLine();
+int[] arrayFiveOne = new int[6] { 0, 2, 9, 4, 3, 11 };
+Console.WriteLine($"Given Array: [0,2,9,4,3,11].");
+Console.Write("Output: ");
+Console.WriteLine(SearchingDoubleNumber.Main(arrayFiveOne));
+
+Console.WriteLine("\n\n06. Valid Mountain Array");
+int[] arraySix = new int[6] { 0, 2, 9, 5, 3, 11 };
+Console.WriteLine($"Given Array: [0,2,9,5,3,11].");
+Console.Write("Output: ");
+Console.WriteLine(ValidMountain.Main(arrayFive));
+Console.WriteLine();
+int[] arraySixOne = new int[10] { 0,1,2,4,10,9,6,3,1,0 };
+Console.WriteLine($"Given Array: [0,1,2,4,10,9,6,3,1,0].");
+Console.Write("Output: ");
+Console.WriteLine(SearchingDoubleNumber.Main(arraySixOne));
+
+
 Console.ReadKey();
