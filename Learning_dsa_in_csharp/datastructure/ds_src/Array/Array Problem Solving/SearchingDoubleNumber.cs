aaee05fe313b3d756519f5112298d64aeb3ab5ae
nomanfc <nomanfcb@gmail.com>
2023-10-18 21:18:29 +0600

    ArrayCP: N and It's Double exist problem

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/SearchingDoubleNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class SearchingDoubleNumber
+    {
+        public static bool Main(int[] array)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            for(int i=0; i<array.Length; i++)
+            {
+                int current = array[i];
+
+                if (set.Contains(current*2) || (current%2 == 0 && set.Contains(current / 2)))
+                {
+                    return true;
+                }
+
+                set.Add(current);
+            }
+
+            return false;
+        }
+    }
+}
