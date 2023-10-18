0d904f921c6f8552bbeae1a55a0be1921fff77eb
nomanfc <nomanfcb@gmail.com>
2023-10-18 16:27:25 +0600

    OOP: Custom Constructor code added

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/CustomConstructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    class Rectangle
+    {
+        public  int Height;
+        public int Width;
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;  
+        }
+    }
+
+}
