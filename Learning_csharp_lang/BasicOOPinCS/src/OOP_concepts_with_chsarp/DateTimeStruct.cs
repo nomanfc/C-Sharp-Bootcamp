3e5fabbe84e930c103042aad2af0486293fb36eb
nomanfc <nomanfcb@gmail.com>
2023-10-18 13:55:48 +0600

    OOP: DateTime structure code added

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/DateTimeStruct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class DateTimeStruct
+    {
+        public static void Main()
+        {
+            var InterNationalMotherLanguageDay = new DateTime(1952, 2, 21);
+
+            Console.WriteLine($"International Mother Language Day is : {InterNationalMotherLanguageDay.Day}");
+            Console.WriteLine($"International Mother Language Month is : {InterNationalMotherLanguageDay.Month}");
+            Console.WriteLine($"International Mother Language Year is : {InterNationalMotherLanguageDay.Year}");
+        }
+    }
+}
