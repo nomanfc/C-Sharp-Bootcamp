03f6de1c7fab5cc4ba333d8fbe9f20fa25621327
nomanfc <nomanfcb@gmail.com>
2023-10-19 19:36:09 +0600

    ArrayCP: Move zeros to the end of an array

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/MoveZerosToEnd.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class MoveZerosToEnd
+    {
+        public static void Main(int[] array)
+        {
+            int length = array.Length;
+
+            int[] copyArray = new int[length];
+
+            int j = 0;
+            for(int i=0; i< array.Length; i++)
+            {
+                if (array[i] != 0)
+                {
+                    copyArray[j] = array[i];
+                    j++;
+                }
+                else
+                {
+                    copyArray[length-1] = array[i];
+                    length--;
+                }
+            }
+
+            for(int i=0; i< array.Length; i++)
+            {
+                array[i] = copyArray[i];
+            }
+
+            Console.Write("Output: ");
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
