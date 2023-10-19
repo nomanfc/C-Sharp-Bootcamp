3dbe66db30d00f28e8a4213b06924d69a05428f2
nomanfc <nomanfcb@gmail.com>
2023-10-19 16:22:49 +0600

    ArrayCP: inplace operation even squared array added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/EvenSquaredArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class EvenSquaredArray
+    {
+        public static void Main(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(i%2 == 0)
+                {
+                    array[i] = array[i] * array[i];
+                }
+            }
+
+            Utility.Display(array);
+        }
+    }
+}
