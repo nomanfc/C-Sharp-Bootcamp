424d508ae7acb3b8dc2ad395cf207f5b0dfc19a1
nomanfc <nomanfcb@gmail.com>
2023-10-19 13:53:03 +0600

    OOP: constructor overloading has been added

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/ConstructorOverloading.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class ConstructorOverloading
+    {
+        private string _patientName;
+        private DateTime _date;
+
+        public ConstructorOverloading(string name, DateTime date)
+        {
+            _patientName = name;
+            _date = date;
+        }
+
+
+        public ConstructorOverloading(string patientName) : this(patientName, 7) { }
+
+        public ConstructorOverloading(string patientNtname, int daysFromNow)
+        {
+            _patientName = patientNtname;
+            _date = DateTime.Now.AddDays(daysFromNow);
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_patientName}\nVisit: {_date}");
+        }
+
+    }
+}
