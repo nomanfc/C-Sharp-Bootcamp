a6b1e19244200e5175d74a0aeb505d09810bff94
nomanfc <nomanfcb@gmail.com>
2023-10-19 14:32:59 +0600

    OOP: This keyword added and Printer calss created

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/ThisKeyword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class Printer
+    {
+        public static void Print(ConstructorOverloading obj)
+        {
+            Console.WriteLine("\n:::::::::::::::::::::::::::::::::::Printed Copy::::::::::::::::::::::::::");
+            Console.WriteLine($"Patient Name is {obj.GetName()}\nHis appoinment will Take Place on {obj.GetDate()}\n");
+        }
+    }
+}
