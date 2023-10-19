96b8eaffcea475e224ec40e192437196ae21e402
nomanfc <nomanfcb@gmail.com>
2023-10-19 18:49:47 +0600

    ArrayCP: Replace array element with right max item added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/ReplaceElementWithRightMax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class ReplaceElementWithRightMax
+    {
+        public static void Main(int[] arr)
+        {
+            int maxRight = -1;
+
+            for(int i = arr.Length-1; i >= 0; i--)
+            {
+                int temp = arr[i];
+                arr[i] = maxRight;
+
+                if(temp > maxRight)
+                {
+                    maxRight = temp;
+                }
+
+            }
+
+            Console.Write("Output: ");
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
