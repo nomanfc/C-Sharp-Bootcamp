5ebf241aea865c642f2318d8f917a478a63e18af
nomanfc <nomanfcb@gmail.com>
2023-10-19 14:02:30 +0600

    OOP: Expression bodied method added ( => )

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/ExpressionBodiedMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class ExpressionBodiedMethod
+    {
+        public int Height;
+        public int Width;
+
+        public ExpressionBodiedMethod(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int CalculateArea() => Height * Width; //Method contains single expression or single statement can be written with Arrow symbol 
+
+        public int CalculateCircumference() => 2 * Height + 2 * Width; //It is called Expression bodied method
+
+    }
+}
