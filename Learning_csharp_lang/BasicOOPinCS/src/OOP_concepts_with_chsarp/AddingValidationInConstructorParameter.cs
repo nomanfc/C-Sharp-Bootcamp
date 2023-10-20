6449a2bef70c3eaacb25ad1e5c703d8c1df9de5d
nomanfc <nomanfcb@gmail.com>
2023-10-20 12:12:42 +0600

    OOP: Validation added in constructor parameter

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/AddingValidationInConstructorParameter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class AddingValidationInConstructorParameter
+    {
+        private int _height;
+        private int _width;
+
+        public AddingValidationInConstructorParameter(int height, int width)
+        {
+            _height = GetLengthOfDefault(height, nameof(_height));
+            _width = GetLengthOfDefault(width, nameof(_width));
+        }
+
+        private int GetLengthOfDefault(int length, string name)
+        {
+            int defaultValueOfNonPositive =  1;
+
+            if(length <= 0)
+            {
+                Console.WriteLine($"{name} must be a positive number.");
+                return defaultValueOfNonPositive;
+            }
+
+            return length;
+        }
+
+        public int CalculateArea()
+        {
+            return _height * _width;
+        }
+
+        public int CalculateCircumference()
+        {
+            return 2 * _height + 2 * _width;
+        }
+
+    }
+}
