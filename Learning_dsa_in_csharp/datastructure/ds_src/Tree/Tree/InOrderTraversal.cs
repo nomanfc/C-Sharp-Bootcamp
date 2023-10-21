de167a83aa54bbabf598e2a5c595c3b2051ac0a8
nomanfc <nomanfcb@gmail.com>
2023-10-21 21:16:07 +0600

    TreeDS: InOrder class mdified

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Tree/Tree/InOrderTraversal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class InOrderTraversal
+    {
+        public static void Inorder(Node root)
+        {
+            if (root != null)
+            {
+                Inorder(root.Left);
+                Console.Write(root.Data + " ");
+                Inorder(root.Right);
+            }
+        }
+    }
+    
+}
