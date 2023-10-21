26fa6927c69cba0b4fddd1802519193abd4973c1
nomanfc <nomanfcb@gmail.com>
2023-10-21 21:17:29 +0600

    TreeDS: Program class updated

--- Learning_dsa_in_csharp/datastructure/ds_src/Tree/Tree/Program.cs
+++ Learning_dsa_in_csharp/datastructure/ds_src/Tree/Tree/Program.cs
@@ -1,4 +1,19 @@
-﻿Console.WriteLine("::::::::::::::::::::- Tree Data Structure -::::::::::::::::::::");
+﻿using Tree;
 
+Console.WriteLine("::::::::::::::::::::- Tree Data Structure -::::::::::::::::::::");
+
+BinaryTree tree = new BinaryTree();
+
+// Insert data into the binary tree
+tree.Insert(50);
+tree.Insert(30);
+tree.Insert(20);
+tree.Insert(40);
+tree.Insert(70);
+tree.Insert(60);
+tree.Insert(80);
+
+Console.WriteLine("Binary Tree Inorder Traversal:");
+InOrderTraversal.Inorder(tree.Root);
 
 Console.ReadKey();
