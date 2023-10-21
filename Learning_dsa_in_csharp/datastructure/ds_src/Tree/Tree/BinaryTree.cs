b7b92051383e92112a5acc4e82e8963b1c0473e1
nomanfc <nomanfcb@gmail.com>
2023-10-21 20:55:57 +0600

    TreeDS: Binary Tree class added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Tree/Tree/BinaryTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class BinaryTree
+    {
+        public Node Root;
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        private void Insert(int data)
+        {
+            Root = InsertRec(Root, data);
+        }
+
+        private Node InsertRec(Node root, int data)
+        {
+            if (root == null)
+            {
+                root = new Node(data);
+                return root;
+            }
+
+            if(root.Data > data)
+            {
+                root.Left = new Node(data);
+            }
+            else if(root.Data < data)
+            {
+                root.Right = new Node(data);
+            }
+
+            return root;
+        }
+    }
+}
