736b3b3932e54139671f3ac3513a4aee83d42f3f
nomanfc <nomanfcb@gmail.com>
2023-10-21 20:55:35 +0600

    TreeDS: Node class added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Tree/Tree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
