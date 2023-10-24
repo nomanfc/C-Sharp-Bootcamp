3ac8d127543b0411d27fe9f146ab1fe17e7e39f0
nomanfc <nomanfcb@gmail.com>
2023-10-24 19:29:32 +0600

    ArrayCP: Largest element as twitch of evey elements

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/LargestNumberTwiceOfOthers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class LargestNumberTwiceOfOthers
+    {
+        public static void Main(int[] nums)
+        {
+            int max = nums[0];
+            int maxIndex = 0;
+            int count = 1;
+
+            for (int i = 1; i<nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                    maxIndex = i;
+                   
+                }
+            }
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (max >= (nums[i] * 2)) 
+                { 
+                    count++;
+                }
+            }
+
+            if(count == nums.Length)
+            {
+                Console.WriteLine($"Max Index: {maxIndex}");
+                Console.WriteLine($"Max Element: {max}");
+            }
+            else
+            {
+                Console.WriteLine("Max Element Not found in Array....!!!");
+            }
+        
+        }
+    }
+}
