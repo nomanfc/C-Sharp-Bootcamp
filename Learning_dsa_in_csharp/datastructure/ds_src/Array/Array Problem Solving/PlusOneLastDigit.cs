1918135ba2c1c43e6020251326837ee1e0674406
nomanfc <nomanfcb@gmail.com>
2023-10-25 13:42:22 +0600

    ArrayCP: Plus One problem added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Array/Array Problem Solving/PlusOneLastDigit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problem_Solving
+{
+    internal class PlusOneLastDigit
+    {
+        public static int[] Main(int[] digits)
+        {
+            Console.Write($"After Plus One: ");
+
+            for (int i=digits.Length-1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i] = digits[i] + 1;
+
+                    return digits;
+                }
+
+                digits[i] = 0;
+            }
+
+
+            int[] result = new int[digits.Length+1];
+            result[0] = 1;
+
+            return result;
+
+
+        }
+    }
+}
