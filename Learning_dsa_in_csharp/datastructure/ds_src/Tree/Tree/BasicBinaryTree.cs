aae1a5aacb28d87200a796e72b561cd3425f376a
nomanfc <nomanfcb@gmail.com>
2023-10-25 21:04:35 +0600

    TreeDS: Baic binary tree class added

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/Tree/Tree/BasicBinaryTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    public class BasicBinaryTreeNode
+    {
+        public char Value;
+        public BasicBinaryTreeNode Left;
+        public BasicBinaryTreeNode Right;
+
+        public BasicBinaryTreeNode(char val)
+        {
+            Value = val;
+            Left = null;
+            Right = null;
+        }
+
+    }
+}
