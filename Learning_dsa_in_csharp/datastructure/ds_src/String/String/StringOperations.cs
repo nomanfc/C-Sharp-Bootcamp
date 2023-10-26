6667263bd2371762de71d074504081ac4250b871
nomanfc <nomanfcb@gmail.com>
2023-10-26 21:56:30 +0600

    StringDS: Comparing to string using ==

--- Learning_dsa_in_csharp/datastructure/ds_src/String/String/StringOperations.cs
+++ Learning_dsa_in_csharp/datastructure/ds_src/String/String/StringOperations.cs
@@ -10,18 +10,19 @@ namespace String
     {
         public static void Main()
         {
-            string s1 = "hello_world string opeartions";
+            string s1 = "hello_word_s1";
             Console.WriteLine($"s1 is : {s1}");
 
             string s2 = s1;
-            Console.WriteLine("string s2 = s1: s2 is another reference to s1.");
+            Console.WriteLine("\nstring s2 = s1: s2 is another reference to s1.");
             Console.WriteLine($"s2 is : {s2}");
 
             string s3 = new string(s2);
-            Console.WriteLine($"string s3 = new string(s2): s3 is a copy of s1.");
+            Console.WriteLine($"\nstring s3 = new string(s2): s3 is a copy of s1.");
             Console.WriteLine($"s3 is : {s3}");
 
-
+            Console.WriteLine("\nComparing using '==' operator:");
+            Console.WriteLine($"s1 == s2 : {s1 == s2}");
         }
     }
 }
