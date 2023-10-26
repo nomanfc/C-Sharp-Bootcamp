eb197bc7697758ce0469e45ba6db53888020b815
nomanfc <nomanfcb@gmail.com>
2023-10-26 21:51:41 +0600

    StringDS: String reference and string copy has been added to String Operationns

--- Learning_dsa_in_csharp/datastructure/ds_src/String/String/StringOperations.cs
+++ Learning_dsa_in_csharp/datastructure/ds_src/String/String/StringOperations.cs
@@ -8,5 +8,20 @@ namespace String
 {
     internal class StringOperations
     {
+        public static void Main()
+        {
+            string s1 = "hello_world string opeartions";
+            Console.WriteLine($"s1 is : {s1}");
+
+            string s2 = s1;
+            Console.WriteLine("string s2 = s1: s2 is another reference to s1.");
+            Console.WriteLine($"s2 is : {s2}");
+
+            string s3 = new string(s2);
+            Console.WriteLine($"string s3 = new string(s2): s3 is a copy of s1.");
+            Console.WriteLine($"s3 is : {s3}");
+
+
+        }
     }
 }
