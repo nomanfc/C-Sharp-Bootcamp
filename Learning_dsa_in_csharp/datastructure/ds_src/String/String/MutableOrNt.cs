643dc7aec3ae0cdeb6f12a243349c15e4d1cde1b
nomanfc <nomanfcb@gmail.com>
2023-10-26 23:48:44 +0600

    StringDS: string is immutable in csharp checking

---
+++ Learning_dsa_in_csharp/datastructure/ds_src/String/String/MutableOrNt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    internal class MutableOrNt
+    {
+        public static void Main()
+        {
+            string s1 = "Hello World";
+            //s1[4] = "1"; //C# string is immutable or read only
+
+        }
+    }
+}
