79943eb75fb6ea572a57dff00987eaa0a935bcdb
nomanfc <nomanfcb@gmail.com>
2023-10-28 21:21:51 +0600

    OOP: Get and Set method has been added

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/GetSetMethod.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class GetSetMethod
+    {
+        private int _height;
+        private int _width;
+
+        public GetSetMethod(int height, int width)
+        {
+            _height = height;
+            _width = width;
+        }
+
+        public int GetHeight()
+        {
+            return _height;
+        }
+        public void SetHeight(int value)
+        {
+            if(value > 0)
+            {
+                _height = value;
+            }
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+        public void SetWidth(int value)
+        {
+            if (value > 0)
+            {
+                _width = value;
+            }
+        }
+        public int CalculateArea()
+        {
+            return _height * _width;
+        }
+
+        public int CalculateCircumference()
+        {
+            return 2 * _height + 2 * _width;
+        }
+    }
+}
