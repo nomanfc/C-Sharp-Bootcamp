40ee7497bc4bb0f5b8e39c2c393d1973f839518a
nomanfc <nomanfcb@gmail.com>
2023-10-29 18:47:03 +0600

    OOP: Manual get set added in Properties

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/Properties.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class Properties
+    {
+        private int _height;
+        private int _width;
+
+        public Properties(int height, int width)
+        {
+            _height = height;
+            _width = width;
+        }
+
+        
+        public int Height
+        {
+            set
+            {
+                if(value > 0)
+                {
+                    _height = value;
+                }
+
+            }
+
+            get
+            {
+                return _height;
+            }
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+        public void SetWidth(int value)
+        {
+            if (value > 0)
+            {
+                _width = value;
+            }
+        }
+        public int CalculateArea()
+        {
+            return _height * _width;
+        }
+
+        public int CalculateCircumference()
+        {
+            return 2 * _height + 2 * _width;
+        }
+    }
+}
