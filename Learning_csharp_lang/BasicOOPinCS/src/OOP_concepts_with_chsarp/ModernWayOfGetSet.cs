7b6bb38604c1a1f04d84ef9da01354c5618bca37
nomanfc <nomanfcb@gmail.com>
2023-10-29 20:13:43 +0600

    OOP: Shorter get set method added

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/ModernWayOfGetSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class ModernWayOfGetSet
+    {
+        public ModernWayOfGetSet(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height { get;  set; }
+
+        public int Width { get; set; }
+        
+        public int CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public int CalculateCircumference()
+        {
+            return 2 * Height + 2 * Width;
+        }
+    }
+}
