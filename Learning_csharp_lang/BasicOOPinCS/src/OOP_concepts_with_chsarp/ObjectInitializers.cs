6089d473efe12887076f9b9ff5bd2a6328c8dccd
nomanfc <nomanfcb@gmail.com>
2023-10-30 19:20:13 +0600

    OOP: Object initializers completed

--- Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/ObjectInitializers.cs
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/ObjectInitializers.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace OOP_concepts_with_chsarp
 {
-    internal class ObjectInitializers
+    internal class Person
     {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+
+        //public Person(string name, int yearOfBirth)
+        //{
+        //    Name = name;
+        //    YearOfBirth = yearOfBirth;
+        //}
     }
 }
