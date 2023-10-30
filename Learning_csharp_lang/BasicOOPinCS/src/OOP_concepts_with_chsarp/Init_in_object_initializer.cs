3d378b3c81dd8dbafe777f7f4f7d4d4c7e01fc01
nomanfc <nomanfcb@gmail.com>
2023-10-30 19:42:23 +0600

    OOP: init for private set added

---
+++ Learning_csharp_lang/BasicOOPinCS/src/OOP_concepts_with_chsarp/Init_in_object_initializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_concepts_with_chsarp
+{
+    internal class Student
+    {
+        public string Name { get; init; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+    }
+}
