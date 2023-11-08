8282c3e0aab8f5d9ba3e5d91b31258c73243f61b
nomanfc <nomanfcb@gmail.com>
2023-11-08 21:42:39 +0600

    String revese function added

---
+++ Coding Interview/coding_interview/StringSolvingTechnique/StringReverse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSolvingTechnique
+{
+    internal class StringReverse
+    {
+        public static void Main()
+        {
+            string st = "Hello World";
+
+            char[] st2 = new char[st.Length];
+
+            int j = 0;
+            for(int i = st.Length - 1; i >= 0; i--)
+            {
+                st2[j] = st[i];
+                j++;
+            }
+
+            Console.WriteLine("Input: " + st);
+            Console.Write("Output: ");
+            foreach (char c in st2)
+            {
+                Console.Write(c);
+            }
+
+        }
+    }
+}
