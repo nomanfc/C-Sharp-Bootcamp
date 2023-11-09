745ec830d7892d616b2921802cb475b77324f696
nomanfc <nomanfcb@gmail.com>
2023-11-09 22:22:14 +0600

    pallingdrom created

--- Coding Interview/coding_interview/StringSolvingTechnique/Program.cs
+++ Coding Interview/coding_interview/StringSolvingTechnique/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿////////////////////// Coding InterView - String Solving ////////////////////
+
+
+using StringSolvingTechnique;
+
+Console.WriteLine("01. String Reverse:");
+StringReverse.Main();
+
+Console.ReadKey();
