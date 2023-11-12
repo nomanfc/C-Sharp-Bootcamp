3d22077b5a957cf0436dce2cffd712d4994960ed
nomanfc <nomanfcb@gmail.com>
2023-11-12 20:57:35 +0600

    Program cs file and build files updated

--- Coding Interview/coding_interview/StringSolvingTechnique/Program.cs
+++ Coding Interview/coding_interview/StringSolvingTechnique/Program.cs
@@ -9,6 +9,12 @@ StringReverse.Main();
 Console.WriteLine("\n\n02. String pallingdrom:");
 string input = "radar radar";
 Console.WriteLine("Input: " + input);
-Console.WriteLine("Pallingdrom: " + Palingdrom.Main(input)); 
+Console.WriteLine("Pallingdrom: " + Palingdrom.Main(input));
+
+Console.WriteLine("\n\n03. Reverse Integer:");
+int number = -1234567890;
+Console.WriteLine("Input: " + number);
+Console.WriteLine("Output: " + ReverseInteger.Main(number));
+
 
 Console.ReadKey();
