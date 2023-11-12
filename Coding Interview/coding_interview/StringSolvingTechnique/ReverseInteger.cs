c9243761845d005b57b4f1ef1ca198b5adb7f7d7
nomanfc <nomanfcb@gmail.com>
2023-11-12 20:57:02 +0600

    ReverseInt Code added

---
+++ Coding Interview/coding_interview/StringSolvingTechnique/ReverseInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSolvingTechnique
+{
+    internal class ReverseInteger
+    {
+        public static int Main(int number)
+        {
+            bool isNegative = number < 0;
+
+            char[] chars = Math.Abs(number).ToString().ToCharArray(); 
+
+            Array.Reverse(chars);
+
+            int reversed = int.Parse(chars);
+
+            return isNegative ? -reversed :  reversed;
+
+        }
+    }
+}
+
+
+//long reversed = 0;
+
+//while (number > 0)
+//{
+//    long reminder = number % 10;
+//    reversed = (reversed * 10) + reminder;
+//    number = number / 10;
+//}
