371fc38136374a877d5a27716864ef463113a30c
nomanfc <nomanfcb@gmail.com>
2023-11-14 22:08:32 +0600

    Program file has been modified

---
+++ Coding Interview/coding_interview/DynamicProblemSolutions/Program.cs
@@ -0,0 +1,11 @@
+﻿using DynammicProblemSolutions;
+
+Console.WriteLine("Dynamic Programming Solutions");
+
+Console.WriteLine("\n01. LCS : Longest Common Subsequence:");
+string inputOne = "acccaaaacaa";
+string inputTwo = "ccaascccaaa";
+Console.WriteLine("Inputs: " + inputOne + " & " + inputTwo);
+Console.WriteLine(LCS.Main(inputOne, inputTwo));
+
+Console.ReadKey();
