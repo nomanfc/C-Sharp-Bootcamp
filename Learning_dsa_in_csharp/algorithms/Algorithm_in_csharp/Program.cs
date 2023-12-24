063ff25fc982a22e8bca076829f788fbc06393a4
Abdullah Al-Noman <nomanfcb@gmail.com>
2023-12-24 21:08:40 +0600

    Dynamic Connectivity Problem: QuickFind Approach added

--- Learning_dsa_in_csharp/algorithms/Algorithm_in_csharp/Program.cs
+++ Learning_dsa_in_csharp/algorithms/Algorithm_in_csharp/Program.cs
@@ -3,7 +3,7 @@ using Boyer_Moore_Voting_algo;
 
 Console.WriteLine("::::::::::::::::::::::::!! Algorithms !!::::::::::::::::::::::");
 
-Console.WriteLine("\n\n1. Boyer Moore Voting Algorithm\n");
+/*Console.WriteLine("\n\n1. Boyer Moore Voting Algorithm\n");
 int[] number = { 0, 0, 0, 0, 0, 2, 2, 0, 1, 2, 1, 0 };
 Console.WriteLine("Array : [0, 0, 0, 0, 0, 2, 2, 0, 1, 2, 1, 0]");
 int winner = BoyerMoreVotingAlgo.VotingAlgo(number);
@@ -14,9 +14,37 @@ if (winner > -1)
 else
 {
     Console.WriteLine("No Winner Found!!");
+}*/
+
+Console.WriteLine("\n2. Union Find Algorithm");
+
+Console.Write("\nEnter Number Of Objects: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+QuickFindAlgorithm QF = new(N);
+
+QF.PrintValue();
+
+while (N > 0)
+{
+    Console.Write("\nEnter P: ");
+    int p = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter Q: ");
+    int q = Convert.ToInt32(Console.ReadLine());
+
+    if (!QF.Connected(p, q))
+    {
+        QF.Union(p, q);
+        Console.WriteLine($"\nConnecting {p} and {q}");
+    }
+    else
+    {
+        Console.WriteLine("\nAlready Connected");
+    }
+
+    QF.PrintValue();
 }
 
-Console.WriteLine("\n\n2. Union Find Algorithm Algorithm\n");
-UnionFindAlgorithm UF = new(16);
-UF.PrintValue();
 Console.ReadLine();
--- Learning_dsa_in_csharp/algorithms/Algorithm_in_csharp/UnionFindAlgorithm.cs
+++ Learning_dsa_in_csharp/algorithms/Algorithm_in_csharp/UnionFindAlgorithm.cs
@@ -6,17 +6,59 @@ using System.Threading.Tasks;
 
 namespace Algorithm_in_csharp
 {
-    public class UnionFindAlgorithm
+    public class QuickFindAlgorithm
     {
-        public int n;
-        public UnionFindAlgorithm(int N)
+        int[] id;
+
+        //this constructor sets objects to ifself O(N)
+        public QuickFindAlgorithm(int N)
         {
-            n = N;
+            id = new int[N];
+
+            for(int i = 0; i < N; i++)
+            {
+                id[i] = i;
+            }
         }
+
+        //checks if p and q are connected components O(1)
+        public bool Connected (int p, int q)
+        {
+            if (id[p] == id[q])
+            {
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }
+        }
+
+        //change all entries with id[p] to id[q] O(2N + 2) or O(N)
+        public void Union(int p, int q)
+        {
+            int pid = id[p];
+            int qid = id[q];
+
+            for(int i = 0; i< id.Length; i++)
+            {
+                if (id[i] == pid)
+                {
+                    id[i] = qid;
+                }
+            }
+
+        }
+
 
         public void PrintValue()
         {
-            Console.WriteLine(n);
+            Console.Write("\nConnection Status: ");
+            for (int i = 0; i < id.Length; i++)
+            {
+                Console.Write(id[i] + " ");
+            }
+            Console.Write("\n\n");
         }
 
     }
