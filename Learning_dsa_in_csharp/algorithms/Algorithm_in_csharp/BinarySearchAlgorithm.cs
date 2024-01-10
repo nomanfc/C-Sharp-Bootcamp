c916fdf14115ee1cb6b2758fefb4f83fd2f3afb1
Abdullah Al-Noman <nomanfcb@gmail.com>
2024-01-10 21:37:08 +0600

    Binary Search added

---
+++ Learning_dsa_in_csharp/algorithms/Algorithm_in_csharp/BinarySearchAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_in_csharp
+{
+    internal class BinarySearchAlgorithm
+    {
+        public static int BinarySearch(int[] array, int key)
+        {
+            int high = array.Length - 1;
+            int low = 0;
+
+            while (low <= high)
+            {
+                int mid = low + ((high - low)/2);
+                if (key < array[mid]) high = mid - 1;
+                else if (key > array[mid]) low = mid + 1;
+                else return mid;
+            }
+
+            return -1; 
+        }
+    }
+}
